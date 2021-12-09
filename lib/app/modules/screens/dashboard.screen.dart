772f7551e8d6d6927626a48be4154b0b0bb1ed97
cedrick <cedrick.sestoso@myxlog.com>
2021-12-09 12:00:18 +0800

    initialized dashboard

---
+++ lib/app/modules/screens/dashboard.screen.dart
@@ -0,0 +1,46 @@
+import 'package:driver_app/app/widgets/widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class DashboardScreen extends StatefulWidget {
+  const DashboardScreen({Key? key}) : super(key: key);
+
+  @override
+  _DashboardScreenState createState() => _DashboardScreenState();
+}
+
+class _DashboardScreenState extends State<DashboardScreen> {
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
+  openDrawer() {
+    _scaffoldKey.currentState?.openDrawer();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+    return Scaffold(
+      key: _scaffoldKey,
+      appBar: MainAppBar(
+        title: Text(
+          'dashboard_label'.tr,
+          style: const TextStyle(
+            fontSize: 15,
+            fontWeight: FontWeight.w500,
+            color: Colors.black87,
+          ),
+        ),
+        onMenuPress: () => openDrawer(),
+        showOnlineButton: true,
+      ),
+      drawer: const MainDrawer(),
+      body: SizedBox(
+        height: size.height,
+        width: size.width,
+        child: Stack(
+          children: const [],
+        ),
+      ),
+    );
+  }
+}
